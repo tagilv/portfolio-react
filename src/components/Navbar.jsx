c6155b977c58e23aae96cd5e38b9b95b52a821c7
Viktor Tagil <viktor.tagil@gmail.com>
2022-07-13 09:07:17 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
-
-// import logo from '../assets/logo.png'
+import avatar from '../assets/avatar.png'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-
+        <img src={avatar} alt="avatar" style={{width: '150px'}} class="absolute left-6 top-7" />
       </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
     </div>
   )
 }
