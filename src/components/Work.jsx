01b8fe248cbe1d2f251e770284691b1c6a9f591a
Viktor Tagil <viktor.tagil@gmail.com>
2022-07-15 15:01:09 +0200

    next adding buttons

---
+++ src/components/Work.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div className='w-full md:h-screen text-[#A8A4CE] bg-[#0a192f] '>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-[#A8A4CE] border-[#A8A4CE]'>Work</p>
+          <p className='py-6'>See some of my recent work</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hoover */}
+            <div>
+              <span>
+
+              </span>
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Work
